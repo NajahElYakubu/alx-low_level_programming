db788e38ae2b778577bff55e4ffeb5fbde8fc43e
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-01 14:08:32 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main -a program that prints the alphabet in lowercase,
+ *followed by a new line
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char var;
+
+	for (var = 'a'; var <= 'z'; var++)
+		if (var == 'e' || var == 'q')
+			continue;
+		putchar var;
+
+	putchar('\n');
+	return (0);
+}

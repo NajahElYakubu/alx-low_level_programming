c70794f05dde559fd95f51ac327b46fcf1aa4718
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-05 16:13:03 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,14 +3,11 @@
 /**
  * add - returns the result of the sum of 2 numbers
  * @a: first variable
- * @b: second variable
- * Return: c ,the sum of the two variables
+ * @b: second varible
+ * Return: returns the sum
  */
 
 int add(int a, int b)
 {
-	int a, b;
-
-	c = int a + int b
-		return (c);
+	return (a + b);
 }

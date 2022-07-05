f71c28510fe555f97e34df0bd912c354a7474720
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-05 14:53:01 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main"
+
+/**
+ * _abs - computes the absolute value of an int
+ * @n: number
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	return ((n <= 0) ? n * -1 : n);
+}

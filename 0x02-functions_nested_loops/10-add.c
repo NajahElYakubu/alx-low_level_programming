eef3a440559246f0b659a6af4f1503f945efc5d2
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-05 16:09:59 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - returns the result of the sum of 2 numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: c ,the sum of the two variables
+ */
+
+int add(int a, int b)
+{
+	int a, b;
+
+	c = int a + int b
+		return (c);
+}

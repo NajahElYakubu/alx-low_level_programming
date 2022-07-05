a2cc26d4a2b2facff343775a92d5b73051271180
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-05 12:56:48 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lowercase
+ * @cc: 6th july 2022
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int j;
+	for(j = 'a'; j =< 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}

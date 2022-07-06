d99c929609526420d18b1b6fdafe128890c62a15
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-06 13:29:49 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	long int n1 = 1, n2 = 2, result, nlast = 98;
 	int x;
 
-	printf("%lu, %lu", n1, n2);
+	printf("%lu, %lu, ", n1, n2);
 	for (x = 2; x < nlast; x++)
 	{
 		result = n1 + n2;

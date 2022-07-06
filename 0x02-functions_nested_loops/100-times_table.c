6407d7373b8eac16b9cc864eebb3b6dcc0c4c45b
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-05 17:51:39 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print - local variable
+ * @var: variable
+ */
+void print(int var)
+{
+	if (var / 10)
+		print(var / 10);
+	_putchar('0' + (var % 10));
+}
+
+/**
+ * print_times_table - to print n times table btw 0 and 14
+ * @n: numbers to print out
+ */
+void print_times_table(int n)
+{
+	int x, y, mul;
+
+	if (n < 0 || n > 15)
+		return;
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			mul = x * y;
+			if (y == 0)
+				_putchar('0' + mull);
+			else if (mul < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + mul);
+			}
+			else if (mul < 100)
+			{
+				_putchar(' ');
+				print(mul);
+			}
+			else
+			{
+				print(mul);
+			}
+			if (jj < n)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

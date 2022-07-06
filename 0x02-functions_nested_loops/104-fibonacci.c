bb99bf4e3b0f3d1b2056ad0219a65c0abb6a5ba4
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-06 16:43:01 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 #define LARGEST 10000000000
 
 /**
- * main - program that prints the first 98 Fibonacci numbers,
+ * main - main block
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
 	unsigned long int hold1, hold2, hold3;
 	int count;
 
-	printf("%lu, %lu", bk1, bk2);
+	printf("%lu, %lu, ", bk1, bk2);
 	for (count = 2; count < 98; count++)
 	{
 		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)

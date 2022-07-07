b89c682804a9ec1f94b30c87956d38c0d99b51aa
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-07 14:29:21 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - main body
+ * @n: variable
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n--)
+			_putchar('_');
+		_putchar('\n');
+	}
+}

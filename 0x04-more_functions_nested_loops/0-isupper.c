71d4c9ff0c10c60da1a6207134e91b7cf922492a
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-07 12:00:54 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - main body
+ * Return: 0 if success, 1 if failure
+ * @c: variable
+ */
+
+int _isupper(int c)
+{
+	if (c <= 'A', c >= 'Z')
+		return (1);
+	else
+		return (0);
+}

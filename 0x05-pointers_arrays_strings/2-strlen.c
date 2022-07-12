0d6d5e5ecff5bee8b252cb1e884ff8b93c267c8f
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-12 07:55:00 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * Return: 0 (Success)
+ * @s: variable
+ */
+
+int _strlen(char *s)
+{
+	len = strlen(s);
+	return (0);
+}

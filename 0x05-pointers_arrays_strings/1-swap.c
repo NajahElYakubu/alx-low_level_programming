1bd09d9132329bfcf0796cced53b69ae32e61f7e
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-12 07:38:55 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main body
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int n1, n2;
+
+	swap_int(&n1, &n2);
+}
+
+/**
+ * swap_int - swap function
+ * @a: first value
+ * @b: second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}

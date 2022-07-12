3ef09efeb6ff0d46a538af5e69107f115baebeeb
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-12 10:59:27 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - main body
+ * Return: 0
+ * @s: variable
+ */
+void rev_string(char *s)
+{
+	int i = strlen(str) - 1, j = 0;
+	char tmp;
+
+	while (i > j)
+	{
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+		i--;
+		j++;
+	}
+}

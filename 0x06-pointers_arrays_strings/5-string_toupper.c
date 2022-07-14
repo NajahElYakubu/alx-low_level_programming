8a2dcdddb5d0512860bd1870b5903aa7e034baa7
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-14 09:21:09 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - main body
+ * @str: string
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= ('a' - 'A');
+		i++;
+	}
+	return (str);
+}

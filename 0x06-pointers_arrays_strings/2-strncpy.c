1711116fcc57f6e3ac4df419ffb241d1c0374916
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-14 07:19:36 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - main body
+ * @dest: string
+ * @src: string
+ * @n: number
+ * Return: result
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}

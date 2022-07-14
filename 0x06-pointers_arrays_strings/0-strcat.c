86f0c09162083079a418e2bfb150c2b00e0dbcc7
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-14 06:28:42 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - main body
+ * @dest:first string
+ * @src: second string
+ * Return: 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	
+	return (0);
+}

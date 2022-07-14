7d9671242ca93066e5969af4fdf94c707361b98f
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-14 07:58:47 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - main body
+ * @s1: string
+ * @s2: string
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

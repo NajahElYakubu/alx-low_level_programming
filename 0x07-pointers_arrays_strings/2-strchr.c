d048d7021be617442d2acced40747a04c97b6d22
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-18 10:08:13 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - main body
+ * @s: string to be scanned
+ * @c: character to be searched
+ * Return: result
+ */
+
+char *_strchr(char *s, char c)
+{
+	/*char *ret;*/
+
+	return(strchr(s, c));
+}

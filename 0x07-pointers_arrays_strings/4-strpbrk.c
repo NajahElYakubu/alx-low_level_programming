ea2b69de786a1f7d2c461dc5ef5f51b8936b66b2
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-18 10:49:23 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strpbrk - main body
+ * @s: string
+ * @accept: string
+ * Return: result
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}

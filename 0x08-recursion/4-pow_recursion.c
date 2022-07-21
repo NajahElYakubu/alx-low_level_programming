95e9b58ebfce30b2b068de78613869b62ca8fc8d
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-21 06:29:39 -0700

    task 3

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - body of function
+ * @x: integer
+ * @y: power
+ * Return: -1 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (pow(x, y));
+
+}

7710023e9cf4a4c801bc60085983de01df7b46e9
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-21 07:22:46 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - body of main function
+ * @n: number
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - body of function
+ * @n: number
+ * @i: integer
+ * Return: -1 if number does not have a natural square root
+ */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}

88f3da39891018a6d50f058e9c13c384c8fefb53
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-22 13:41:27 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main body
+ * @argc: arguments
+ * @argv: argument array
+ * Return: Success
+ */
+int main(int argc, char **argv)
+{
+	int x. y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
+
+	return (0);
+}
+
+

c2f9183e9be845dfb63d23769b3e86d409632a7f
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-22 13:18:42 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -main body
+ * @argc: argument count
+ * @argv: argument array
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

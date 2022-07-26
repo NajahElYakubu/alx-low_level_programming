294a9c6f43216fe47ef28d5243fbb7432e8a0686
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-07-26 03:53:22 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - body of funtion
+ * @str: string
+ * Return: copy of str
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		i++;
+	copy = (char *)malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[len] = '\0';
+
+	return (copy);
+}

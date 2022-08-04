0df547bf473ebbde1344f1cd63e5fb5ef9cf2d6c
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-08-04 15:34:10 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,20 +3,20 @@
 
 /**
  * print_numbers - body of function
- * @seperator: string
+ * @separator: string
  * @n: number
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int;
+	unsigned int i;
 	va_list list;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (i != (n - 1) && seperator != NULL)
-			printf("%s", seperator)
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 
